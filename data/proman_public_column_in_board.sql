7000c9948a9d12fc9d027b92a41e9d129d523e06
bodroghykristof <bodroghykristof@gmail.com>
2020-08-01 22:01:33 +0200

    Add data folder with sql files in it

---
+++ data/proman_public_column_in_board.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (2, 7, 1);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (2, 8, 2);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (2, 10, 3);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (2, 11, 4);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (5, 7, 1);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (5, 8, 2);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (5, 10, 3);
+INSERT INTO public.column_in_board (board_id, status_id, order_by_position) VALUES (5, 11, 4);
