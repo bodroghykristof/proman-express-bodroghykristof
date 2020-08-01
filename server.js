fa5f47f36554e9bc9552bfc5704afe2144f2810c
bodroghykristof <bodroghykristof@gmail.com>
2020-08-01 23:17:22 +0200

    Serve static files (change url_for references to static routes

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ import express from "express";
 import pool from "./database.js"
 
 const app = express();
+app.use(express.static('public'));
 const port = 3000;
 
-app.get('/', async (req, res) => {
-    try {
-        console.log('Get');
-        const allData = await pool.query('SELECT title FROM card');
-        res.send(allData.rows);
-    } catch (error) {
-        console.log('An error occured: ' + error)
-    }
-})
+// app.get('/', async (req, res) => {
+//     try {
+//         console.log('Get');
+//         const allData = await pool.query('SELECT title FROM card');
+//         res.send(allData.rows);
+//     } catch (error) {
+//         console.log('An error occured: ' + error)
+//     }
+// })
 
 app.listen(port, () => console.log(`Server initialized on port ${port}`));
